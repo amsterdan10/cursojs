b3bbd27aa2a3e269f6ed0ab2f9b3ad0194349317
amsterdan10 <119135658+amsterdan10@users.noreply.github.com>
2023-03-13 16:39:39 -0300

    update

--- exe_e_des_youtube/desafios_youtube/d005/js/script.js
+++ exe_e_des_youtube/desafios_youtube/d005/js/script.js
@@ -20,9 +20,42 @@ function isLista(n, l) {
 
 function adicionar() {
     if (isNumero(num.value) && !isLista(num.value, valores)) {
-        alert('oi')
+        valores.push(Number(num.value))
+        var item = document.createElement('option')
+        item.text = `Valor ${num.value} adcionado.`
+        list.appendChild(item)
+        res.innerHTML = ''
     } else {
         alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+        var total = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var somar = 0
+        var media = 0
+        for (let pos in valores) {
+            somar += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor) 
+                menor = valores[pos]
+        }
+        media = somar / valores.length
+
+        res.innerHTML = `<p>Ao todo temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
+        res.innerHTML += `<p>A soma dos valores informados é igual a ${somar}.</p>`
+        res.innerHTML += `<p>A média dos valores informados é igual a ${media}.</p>`
+
     }
 }
 
